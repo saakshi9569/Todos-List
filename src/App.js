50bc5bc0a577eec0a103f1d438130b88c5fcc100
sakshiSharma <sakshisharma7269@gmail.com>
2023-07-31 00:19:50 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+//import statement and default so we don't use {}
+import Header from "./My Components/Header";
+//we use function 
+import { Todos } from "./My Components/Todos";
+import { Footer } from "./My Components/Footer";
+import { AddTodo } from "./My Components/AddTodo";
+import {About} from "./My Components/About";
+//for deleting in react we use hooks
+//import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import {BrowserRouter as Router,  Switch, Route} from "react-router-dom";
 
 function App() {
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    //Incase of null
+    initTodo = [];
+  }
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const onDelete = (todo) => {
+    console.log("I am ondelete of todo", todo);
+    // Deleting does not work in this way in react
+    // let index = todos.indexOf(todo);
+    // todos.splice(index,1);
+
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+  const addTodo = (title, desc) => {
+    console.log("I am adding this todo ", title, desc)
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
+    }
+    else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+
+
+  }
+
+  const [todos, setTodos] = useState(initTodo);
+
+  //this is used to store new todos
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header title="My Todos List" searchBar={false} />
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/" render={()=>{
+            return(
+            <>
+            <AddTodo addTodo={addTodo} />
+            <Todos todos={todos} onDelete={onDelete} />
+            </>)
+          }}>
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
---
+++ src/My Components/Todos.js
@@ -0,0 +1,29 @@
+//rafc
+import React from 'react'
+import {TodoItem} from "./TodoItem";
+
+
+export const Todos = (props) => {
+  let myStyle={
+    minHeight: "70vh",
+    margin: "10px auto"
+  }
+  return (
+  //   <div className="container">
+  //   <h3 className="text-center">Todos List</h3>
+  //   {props.todos.map((todo) => (
+  //     <TodoItem key={todo.sno} todo={todo} />
+  //   ))}
+  // </div>
+    <div className="container " style={myStyle}>
+        <h3 className="my-3">Todos List</h3>
+        {props.todos.length===0?"No Todos to display":
+        props.todos.map((todo)=>{
+           return (
+            <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
+            )
+        })
+        }
+    </div>
+  )
+}
